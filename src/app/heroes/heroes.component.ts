bbcce59cb313ffda3c37ded4f17f676f4acbf161
unknown <isabqm121@gmail.com>
2023-11-03 12:13:58 -0400

    añadir lista de heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { Hero } from '../hero'; //importamos la interface
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  hero: Hero={
-    id:1,
-    name:'Windstorm'
-  };
+  heroes=HEROES;
 }
