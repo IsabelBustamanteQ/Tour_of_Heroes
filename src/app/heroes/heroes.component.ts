29d26601d679a80975a2e3abe8dccfc00c9d4c9a
unknown <isabqm121@gmail.com>
2023-11-04 18:08:53 -0400

    se añade un servicio

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero'; //importamos la interface
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent {
-  heroes=HEROES;
+  heroes:Hero[]=[];
   selectedHero?:Hero;
   onSelect(hero:Hero): void{
     this.selectedHero=hero;
+  }
+  constructor(private heroService:HeroService){}
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
+  ngOnInit():void{
+    this.getHeroes();
   }
 }
