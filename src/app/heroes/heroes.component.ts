6d76c539f1b8ceb4f94053cabd7b2baaf1998de2
unknown <isabqm121@gmail.com>
2023-11-04 22:20:51 -0400

    se añadió mensajes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,23 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Hero } from '../hero'; //importamos la interface
-import { HEROES } from '../mock-heroes';
+import { MessageService } from '../message.service';
 import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
   heroes:Hero[]=[];
   selectedHero?:Hero;
-  onSelect(hero:Hero): void{
-    this.selectedHero=hero;
-  }
-  constructor(private heroService:HeroService){}
-  getHeroes():void{
-    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
-  }
+  constructor(private heroService:HeroService, private messsageService:MessageService){}
   ngOnInit():void{
     this.getHeroes();
   }
+  onSelect(hero:Hero): void{
+    this.selectedHero=hero;
+    this.messsageService.add('HeroesComponent: Selected hero id=${hero.id}');
+  }
+  getHeroes():void{
+    this.heroService.getHeroes().subscribe(heroes=>this.heroes=heroes);
+  }
+  
 }
